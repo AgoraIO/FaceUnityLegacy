172c983e5b0dce1501f343de1f2c756d2d9e63c0
Minming Qi <alexqm2@aliyun.com>
2019-05-22 14:22:23 +0800

    Upgrade FaceUnity SDK to V6.0

--- Agora-Video-With-FaceUnity-Android/app/src/main/java/com/faceunity/fulivedemo/entity/EffectEnum.java
+++ Agora-Video-With-FaceUnity-Android/app/src/main/java/com/faceunity/fulivedemo/entity/EffectEnum.java
@@ -70,14 +70,13 @@ public enum EffectEnum {
 //    Effect_portraitLighting_X_rim("PortraitLighting_X_rim", R.drawable.portrait_lighting_x_rim, "portrait_light/PortraitLighting_X_rim.bundle", 1, Effect.EFFECT_TYPE_PORTRAIT_LIGHT, 0),
 //    Effect_portraitLighting_X_studio("PortraitLighting_X_studio", R.drawable.portrait_lighting_x_studio, "portrait_light/PortraitLighting_X_studio.bundle", 1, Effect.EFFECT_TYPE_PORTRAIT_LIGHT, 0),
 
-//    Effect_huangya_Animoji("huangya_Animoji", R.drawable.huangya_animoji, "animoji/huangya_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
+    Effect_huangya_Animoji("huangya_Animoji", R.drawable.huangya_animoji, "animoji/huangya_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
     Effect_douniuquan_Animoji("douniuquan_Animoji", R.drawable.douniuquan_animoji, "animoji/douniuquan_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
     Effect_hashiqi_Animoji("hashiqi_Animoji", R.drawable.hashiqi_animoji, "animoji/hashiqi_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
     Effect_hetun_Animoji("hetun_Animoji", R.drawable.hetun_animoji, "animoji/hetun_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
     Effect_baimao_Animoji("baimao_Animoji", R.drawable.baimao_animoji, "animoji/baimao_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
-    Effect_chaiquan_Animoji("chaiquan_Animoji", R.drawable.chaiquan_animoji, "animoji/chaiquan_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
-    Effect_kulutou_Animoji("kulutou_Animoji", R.drawable.kulutou_animoji, "animoji/kulutou_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
-    Effect_maotouying_Animoji("maotouying_Animoji", R.drawable.maotouying_animoji, "animoji/maotouying_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
+    Effect_kuloutou_Animoji("kuloutou_Animoji", R.drawable.kuloutou_animoji, "animoji/kuloutou_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
+    Effect_frog_Animoji("frog_Animoji", R.drawable.frog_st_animoji, "animoji/frog_Animoji.bundle", 1, Effect.EFFECT_TYPE_ANIMOJI, 0),
 
     Effect_picasso_e1("picasso_e1", R.drawable.picasso_e1, "portrait_drive/picasso_e1.bundle", 1, Effect.EFFECT_TYPE_PORTRAIT_DRIVE, 0),
     Effect_picasso_e2("picasso_e2", R.drawable.picasso_e2, "portrait_drive/picasso_e2.bundle", 1, Effect.EFFECT_TYPE_PORTRAIT_DRIVE, 0),
