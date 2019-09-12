f13930ec6f0ad9b26845f774d3b091ad096f5a83
yangshangbin <1058070112@qq.com>
2019-09-12 11:20:37 +0800

    修改切换摄像头旋转问题

--- Agora-Video-With-FaceUnity-iOS/AgoraWithFaceunity/Controllers/RoomViewController.m
+++ Agora-Video-With-FaceUnity-iOS/AgoraWithFaceunity/Controllers/RoomViewController.m
@@ -98,25 +98,7 @@
 
 - (void)viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
-    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
 
-    switch (orientation) {
-        case UIInterfaceOrientationPortrait:
-            [self.mCamera setCaptureVideoOrientation:AVCaptureVideoOrientationPortrait];
-            break;
-        case UIInterfaceOrientationPortraitUpsideDown:
-            [self.mCamera setCaptureVideoOrientation:AVCaptureVideoOrientationPortraitUpsideDown];
-            break;
-        case UIInterfaceOrientationLandscapeLeft:
-            [self.mCamera setCaptureVideoOrientation:AVCaptureVideoOrientationLandscapeLeft];
-            break;
-        case UIInterfaceOrientationLandscapeRight:
-            [self.mCamera setCaptureVideoOrientation:AVCaptureVideoOrientationLandscapeRight];
-            break;
-
-        default:
-            break;
-    }
 }
 
 - (void)viewDidLayoutSubviews {
@@ -547,6 +529,7 @@
     //Change camera need to call below function
     [self.agoraKit switchCamera];
     [[FUManager shareManager] onCameraChange];
+    [self setCaptureVideoOrientation];
 }
 
 - (IBAction)muteBtnClick:(UIButton *)sender {
@@ -557,6 +540,27 @@
 
 - (IBAction)buglyBtnClick:(UIButton *)sender {
     self.buglyLabel.hidden = !self.buglyLabel.hidden;
+}
+
+- (void)setCaptureVideoOrientation {
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+
+    switch (orientation) {
+        case UIInterfaceOrientationPortrait:
+            [self.mCamera setCaptureVideoOrientation:AVCaptureVideoOrientationPortrait];
+            break;
+        case UIInterfaceOrientationPortraitUpsideDown:
+            [self.mCamera setCaptureVideoOrientation:AVCaptureVideoOrientationPortraitUpsideDown];
+            break;
+        case UIInterfaceOrientationLandscapeLeft:
+            [self.mCamera setCaptureVideoOrientation:AVCaptureVideoOrientationLandscapeLeft];
+            break;
+        case UIInterfaceOrientationLandscapeRight:
+            [self.mCamera setCaptureVideoOrientation:AVCaptureVideoOrientationLandscapeRight];
+            break;
+        default:
+            break;
+    }
 }
 
 #pragma mark --- FUItemsViewDelegate
